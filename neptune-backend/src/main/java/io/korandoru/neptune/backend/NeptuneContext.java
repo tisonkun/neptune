7af78b4ad8a3a5057d2965c54eb0d074f27e4dea
tison <wander4096@gmail.com>
2022-06-15 09:50:58 +0800

    feat: clickhouse driver
    Signed-off-by: tison <wander4096@gmail.com>

--- neptune-backend/src/main/java/io/korandoru/neptune/backend/NeptuneContext.java
+++ neptune-backend/src/main/java/io/korandoru/neptune/backend/NeptuneContext.java
@@ -17,27 +17,28 @@
 package io.korandoru.neptune.backend;
 
 import javax.sql.DataSource;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@SpringBootTest
-class NeptuneBackendApplicationTests {
+@Configuration
+@Profile("production")
+public class NeptuneContext {
 
     @Bean
-    @Profile("test")
     public DataSource dataSource() {
         final var dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db");
-        dataSource.setUsername("neptune");
+        dataSource.setDriverClassName("com.clickhouse.jdbc.ClickHouseDriver");
+        dataSource.setUrl("jdbc:ch://play.clickhouse.com:443");
+        dataSource.setUsername("explorer");
         return dataSource;
     }
 
-    @Test
-    void contextLoads() {
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
 }
